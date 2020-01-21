bb5544980aa8b1e4edf0cf8a9ca60057dfd70ebe
Jaimil <jaimilpatel30@gmail.com>
2020-01-21 15:28:06 -0800

    majority of lower case letters added. Final Project

--- Password_Generator/main.py
+++ Password_Generator/main.py
@@ -1,47 +1,40 @@
 import random
 import string
 
+print('\n')
 print("Enter the desired length of password: \n")
+
 pass_length = input()
 pass_length = int(pass_length)
 
-lcase_length = int(pass_length/4)
 ucase_length = int(pass_length/4)
 char_length  = int(pass_length/4)
+digit_length = int(pass_length/4)
+lcase_length = pass_length - digit_length - ucase_length - char_length
 
-remaining = pass_length - lcase_length - ucase_length - char_length
-
-
+#lower case letters
 lcase = random.sample(string.ascii_lowercase, lcase_length)
+
+#upper case letters
 ucase = random.sample(string.ascii_uppercase, ucase_length)
 
-#digits = random.sample(range(10000000), remaining)
+#digits from 0 to 9
 digits = []
-for i in range(remaining):
+for i in range(digit_length):
     number = str(random.randint(0,9))
     digits.append(number)
 
+#selected characters
 characters = []
 for i in range(char_length):
     character = random.choice("~!@#$%^&*()_+=-")
     characters.append(character)
 
-#print(characters)
-
-
-#print(type(digits))
-#print("\n")
-#print(type(lcase))
-#print("\n")
-#print(type(ucase))
-#print("\n")
-
 password = lcase + ucase + digits + characters
 
-random.shuffle(password)
+random.shuffle(password) #shuffle all the characters of the password
 
+print('\n')
 print("Your password is : ")
 print("".join(password))
 print('\n')
-
-
